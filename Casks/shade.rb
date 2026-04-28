bb6bec89d3450a315aec98245b286e9aaea0ebb2
Tim Havlicek <tihav@sv-mac-0153.local>
2026-04-28 11:07:41 +0200

    style: fix cask formatting

--- Casks/shade.rb
+++ Casks/shade.rb
@@ -1,11 +1,9 @@
 cask "shade" do
   version "0.1.0"
   sha256 "5a21246598736806ac12c47314f4768029cc690254fdd4f6c4f5ac868bfe3800"
-  
+
   url "https://github.com/luckydye/shade/releases/download/v2026-04-26/Shade_0.1.0_aarch64.dmg"
   name "Shade"
   desc "Focus and productivity tool"
   homepage "https://github.com/luckydye/shade"
-
-  # Standard DMG installation
 end
